57ff3ccfbdcf517e76d98b0c85eeb67b7b2296c5
andreig992 <andreig992@gmail.com>
2017-08-18 21:38:55 -0400

    redesign

--- javascript.js
+++ javascript.js
@@ -2,31 +2,39 @@
 var isOpen = false;
 
 function openNavAction() {
-    if ((window.innerWidth / window.innerHeight) < 1) {
-        document.getElementById("mySidenav").style.width = "100%";
-    } else if ((window.innerWidth / window.innerHeight) < 1.2) {
-        document.getElementById("mySidenav").style.width = "50%";
-    } else if ((window.innerWidth / window.innerHeight) < 1.7) {
-        document.getElementById("mySidenav").style.width = "35%";
-    } else {
-        document.getElementById("mySidenav").style.width = "25%";
-    }
+  pushHHS();
+  if ((window.innerWidth / window.innerHeight) < 1) {
+    document.getElementById("mySidenav").style.width = "100%";
+  } else if ((window.innerWidth / window.innerHeight) < 1.2) {
+    document.getElementById("mySidenav").style.width = "50%";
+  } else if ((window.innerWidth / window.innerHeight) < 1.7) {
+    document.getElementById("mySidenav").style.width = "35%";
+  } else {
+    document.getElementById("mySidenav").style.width = "25%";
+  }
 }
 
 function openNav() {
-    openNavAction();
-    isOpen = true;
+  openNavAction();
+  isOpen = true;
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    isOpen = false;
+  document.getElementById("mySidenav").style.width = "0";
+  isOpen = false;
 }
 
 window.addEventListener('resize', function(event) {
-    if (isOpen == true) {
-        openNavAction();
-    }
-
+  if (isOpen === true) {
+    openNavAction();
+  }
 });
+
+function pushHHS() {
+  document.getElementById("HHSLogo").style.visibility = "hidden";
+}
+
+window.onload = function(){
+    document.getElementById('vid').play();
+}
