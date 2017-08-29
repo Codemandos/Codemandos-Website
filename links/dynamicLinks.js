c1f2d2df977e5e7cf8e32a1a9e7d17cc69e11fc8
Andrei Cozma <andreig992@gmail.com>
2017-08-29 16:58:32 -0500

    Redone dyn links

--- links/dynamicLinks.js
+++ links/dynamicLinks.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
   $.getJSON("https://spreadsheets.google.com/feeds/list/18af0_kpv1mb6P-6-o9-r8Z7AO4f-pkm8yMqpoZEPwlE/od6/public/values?alt=json", function(data) {
     //first row "title" column
-    var entries = data.feed.entry;
-    var titleLinkRaw = data.feed.entry[0]['content']['$t'];
-    var descriptionRaw = data.feed.entry[1];
+    $('ul').prepend('<br>');
 
-    var titleLinkFiltered = titleLinkRaw.split(', ');
+    var titleRaw = data.feed.entry[0]['content']['$t'];
+    var linkRaw = data.feed.entry[1]['content']['$t'];
+    var descriptionRaw = data.feed.entry[2]['content']['$t'];
 
-
+    var titleFiltered = titleRaw.split(', ');
+    var linkFiltered = linkRaw.split(', ');
+    var descriptionFiltered = descriptionRaw.split(', ');
+    //  console.log(titleFiltered);
     var curatedData = [];
-    for (i = 0; i < titleLinkFiltered.length; i++) {
-      curatedData.push(titleLinkFiltered[i].split(": "));
-
+    for (i = 0; i < titleFiltered.length; i++) {
+      var entry = titleFiltered[i].split(": ");
+      entry.push(linkFiltered[i].split(": ")[1]);
+      entry.push(descriptionFiltered[i].split(": ")[1]);
+      curatedData.push(entry);
     }
 
-    for (x = 0; x < curatedData.length; x++) {
-      curatedData[x][2] = descriptionRaw['gsx$' + curatedData[x][0]]['$t'];
-    }
-    console.log(curatedData);
     for (z = 0; z < curatedData.length; z++) {
-      var newEl = $("<a class='dynID' href='" + curatedData[z][1] + "'></a>").text(curatedData[z][0].charAt(0).toUpperCase() + curatedData[z][0].slice(1) + " - " + curatedData[z][2]);
-      console.log(curatedData[z][1]);
+      if (!curatedData[z][2].includes("http")) {
+        curatedData[z][2] = "http" + curatedData[z][2];
+      }
+      var newEl = $(('<a id="' + curatedData[z][0].slice(1) + '" href="' + curatedData[z][2] + '"></a>')).append($("<font color='gold'></font>").text(curatedData[z][1])).append($("<i></i>").text(" - " + curatedData[z][3]));
       $('ul').prepend(newEl);
+      $('#' + curatedData[z][0].slice(1)).wrap("<li></li>");
+
     }
-    $(".dynID").wrap("<li></li>");
+
   });
 })
